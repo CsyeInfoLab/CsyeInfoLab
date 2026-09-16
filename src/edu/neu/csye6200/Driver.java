1b8268d71fdd4e49388f33d9c67d10edbb1b809c
venugopalThejashvi <venugopal.t@northeastern.edu>
2026-09-16 19:18:46 -0400

    Update Driver.java

--- src/edu/neu/csye6200/Driver.java
+++ src/edu/neu/csye6200/Driver.java
@@ -1,7 +1,12 @@
 package edu.neu.csye6200;
 
+import edu.neu.csye6200.test.Test;
+
 public class Driver {
+
     public static void main(String[] args) {
-          System.out.println("Hello World");
+
+        Test.sayHello();
+
     }
 }
